a3409568aaa508deb3acd8158dd6220f48b82c3c
Berkay <senkazanberkay@gmail.com>
2024-11-09 19:19:58 +0300

    uvicorn

--- whitesmith/__main__.py
+++ whitesmith/__main__.py
@@ -1,9 +1,9 @@
 """Question generation and evaluation"""
 from whitesmith.cli import cli
 import applicat
-import gunicorn
+import uvicorn
 
 if __name__ == "__main__":
     ##cli()
-    gunicorn.run(applicat, host= "0.0.0.0", port="8000")
+    uvicorn.run(applicat, host= "0.0.0.0", port="8000")
     
